eaa74051a8669265430d6f5652a1e267bbb8df13
Ninellie <a.pawlow2009@ya.ru>
2024-08-09 19:44:46 +0300

    Added ScanTaskProvider into separate file

---
+++ ScanTaskProvider.cs
@@ -0,0 +1,27 @@
+﻿namespace MessageReader;
+
+public class ScanTaskProvider
+{
+    /// <summary>
+    /// Вовзращает лист задач на сканирование всего вчерашнего дня
+    /// </summary>
+    /// <param name="group"></param>
+    /// <returns></returns>
+    public List<ScanTask> GetYesterdayTasks(string group)
+    {
+        var tasks = ScanTask.GetScanTasks(group, 24, DateTime.Today);
+        return tasks;
+    }
+
+    /// <summary>
+    /// Возвращает задачи на сканирование временного периода текущего часа вчерашнего дня.
+    /// </summary>
+    /// <param name="group"></param>
+    /// <returns></returns>
+    public List<ScanTask> GetCurrentHourYesterdayTasks(string group)
+    {
+        var dateFrom = DateTime.Today.AddDays(-1).AddHours(DateTime.Now.Hour);
+        var tasks = ScanTask.GetScanTasks(group, 1, dateFrom);
+        return tasks;
+    }
+}
--- ScanTimerService.cs
+++ ScanTimerService.cs
@@ -84,29 +84,3 @@ public class ScanTimerService : BackgroundService
         return DateTime.Today.AddHours(1) - DateTime.Now;
     }
 }
-
-public class ScanTaskProvider
-{
-    /// <summary>
-    /// Вовзращает лист задач на сканирование всего вчерашнего дня
-    /// </summary>
-    /// <param name="group"></param>
-    /// <returns></returns>
-    public List<ScanTask> GetYesterdayTasks(string group)
-    {
-        var tasks = ScanTask.GetScanTasks(group, 24, DateTime.Today);
-        return tasks;
-    }
-
-    /// <summary>
-    /// Возвращает задачи на сканирование временного периода текущего часа вчерашнего дня.
-    /// </summary>
-    /// <param name="group"></param>
-    /// <returns></returns>
-    public List<ScanTask> GetCurrentHourYesterdayTasks(string group)
-    {
-        var dateFrom = DateTime.Today.AddDays(-1).AddHours(DateTime.Now.Hour);
-        var tasks = ScanTask.GetScanTasks(group, 1, dateFrom);
-        return tasks;
-    }
-}
